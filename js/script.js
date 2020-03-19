ffe47b2a53fd19f49dfcfe2e3d272c10e2f0793b
IvyWesselink <55988292+IvyWesselink@users.noreply.github.com>
2020-03-19 23:02:43 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,6 @@ let svg = d3.select("body").append("svg")
         .attr("width", 500)
         .attr("height", 500)
 
-        console.log(myData);
-
-
-
     d3.select('svg')
         .selectAll('circle')
         .data(myData)
