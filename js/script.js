447a399fa87b2a613bb431ca399050ea21c4d7c5
IvyWesselink <55988292+IvyWesselink@users.noreply.github.com>
2020-03-19 22:54:46 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let myData = [1,2,3,4,5];
+
+let svg = d3.select("body").append("svg")
+
+    svg
+        .attr("width", 500)
+        .attr("height", 500)
+
+        console.log(myData);
+
+
+
+    d3.select('svg')
+        .selectAll('circle')
+        .data(myData)
+        .enter()
+        .append('circle')
+        .attr('r', 25)
+        .style('fill', (d,i) => {
+            if ( i % 2 == 0 ) { 
+                return "green"
+            } else {
+                return "red";
+            }
+        })
+        .attr('cx', function(d){
+            return d * 25
+        })
+        .attr('cy', function(d){
+            return d * 25
+        })
+        
